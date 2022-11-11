47853c0796fb920f47abb6e7a4809a517bb077aa
Master Bateson <kmcumfap@gmail.com>
2022-11-12 01:33:02 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+with open('requirements.txt') as requirements_file:
+    install_requirements = requirements_file.read().splitlines()
+
+setup(
+    name="eht-dl",
+    version="0.3.0",
+    description="eht-dl",
+    author="mstrbtsn",
+    packages=find_packages(),
+    install_requires=install_requirements,
+    entry_points={
+        "console_scripts": [
+            "eht-dl=eht_dl.__main__:main",
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.10.6',
+    ]
+)
